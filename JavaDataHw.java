f1ce1377beafe233fff4f1abb025669424aeb21e
dd482IT <dd482@njit.edu>
2020-09-15 20:29:12 -0400

    Java Data Homework

---
+++ JavaDataHw.java
@@ -0,0 +1,18 @@
+public class JavaDataHw {
+	public static void main(String[] args)
+	{
+		// If it matches my sample exactly it gets a 0
+		// 1. Create a variable that holds a name
+			String name = "Daniel Daszkiewicz";
+		// 2. Output the name to the console
+			System.out.println(name);
+		// 3. Create a variable that holds an age (as a whole number)
+			int age = 20;
+		// 4. Output the age to the console
+			System.out.println(age);
+		// 5. Create a variable that holds a decimal
+			float pie = 3.14f;
+		// 6. Output the variable
+			System.out.println(pie);
+	}
+}
