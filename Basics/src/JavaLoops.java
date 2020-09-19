58135736991b777028af48ff73c62b0c49f01a89
dd482IT <dd482@njit.edu>
2020-09-19 11:08:34 -0400

    Java Loops Homework

---
+++ Basics/src/JavaLoops.java
@@ -0,0 +1,15 @@
+
+public class JavaLoops {
+
+	public static void main(String[] args) {
+		
+		int[] nums = {1,2,3,4,5,6};
+		
+		for(int i = 0; i < nums.length; i++)
+		{
+			if(nums[i]%2 == 0){System.out.println(nums[i]);}
+		}
+
+	}
+
+}
