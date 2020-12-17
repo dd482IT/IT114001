e3989b6a0a36a9abdd2e7b99eaba043e18bb6f69
dd482IT <dd482@njit.edu>
2020-12-16 21:28:47 -0500

    Added Exporting Chat Feature

---
+++ ChatroomM4/client/client/User.java
@@ -0,0 +1,29 @@
+package client;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class User extends JPanel {
+	private String name;
+	private JTextField nameField;
+
+	public User(String name) {
+		this.name = name;
+		nameField = new JTextField(name);
+		nameField.setEditable(false);
+		this.setLayout(new BorderLayout());
+		this.add(nameField);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	// Added for feature to show user muted on client List
+	public void setName(String name, String wrapper) {
+		nameField.setText(String.format(wrapper, name));
+	}
+
+}
